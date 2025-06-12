b829d01bea2e9f22501acbe64973d65769fd6f3d
Daniel Mello <danmarmel07@gmail.com>
2025-06-12 13:05:46 -0300

    Add files via upload

---
+++ Site_1/index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+<body>      
+
+    <div class="container-fluid py-3 borderr bg-primary text-light text-start">
+        <img src="fotos/logo.png" alt="" style="width: 160px; height: 75px;" class="figure-img img-fluid rounded ">
+    </div>
+
+    <div class="container-fluid py-2 bg-dark text-light">
+        <div class="container ">
+            <div class="row">
+                <div class="col-auto">
+                    <a href="" class="text-light text-decoration-none">Página Inicial</a>
+                </div>
+                <div class="col-auto">
+                    <a href="#sobre" class="text-light text-decoration-none">Sobre nós</a>
+                </div>
+                <div class="col-auto">
+                    <a href="#avaliacao" class="text-light text-decoration-none">Avalie-nos</a>
+                </div>
+                <div class="col text-end">
+                    <a href="" class="text-light text-decoration-none">Cadastre-se</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid text-center my-5">
+        <h1>A importância da tecnologia em nossa vida: como ela impacta nosso dia-a-dia</h1>
+        
+        <div class="row">
+            <div class="col-3">
+                <p class="fs-2">Promoções:</p>
+                <div class="card my-5">
+                    <img src="https://static.vecteezy.com/ti/vetor-gratis/p1/11765951-modelo-de-smartphone-branco-apple-iphone-14-pro-novidade-da-industria-papel-de-parede-original-maquete-para-web-design-em-um-fundo-branco-gratis-vetor.jpg" class="card-img-top " alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Preços imperdíveis</h5>
+                        <p class="card-text">Celulares da Apple com até 50% de desconto</p>
+                        <p class="card-text">Compre do melhor!</p>
+                        <a href="#" class="btn btn-primary">VER</a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="https://i.zst.com.br/thumbs/45/34/1e/-1449381969.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Preços imperdíveis</h5>
+                        <p class="card-text">Celulares da Xiaomi com até 45% de desconto</p>
+                        <p class="card-text">Compre dos melhores!</p>
+                        <a href="#" class="btn btn-primary">VER</a>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-6">
+
+                
+                    <p>
+                        A tecnologia sempre teve um papel importante na vida das pessoas. Desde as invenções mais antigas, como a roda, até os avanços modernos, ela veio para facilitar o dia a dia. Com o tempo, foi ajudando não só no trabalho, mas também na forma como as pessoas se comunicam, aprendem e se conectam. <br>
+
+                        Um exemplo claro disso é o celular. No começo, ele só servia para fazer ligações. Hoje, ele virou quase uma extensão do corpo humano. É com ele que muita gente trabalha, estuda, se diverte, se informa e fala com quem está longe. De forma discreta, o celular virou companheiro de todas as horas — está no bolso, na mão, na mesa, sempre por perto. <br>
+                        <img src="https://static.vecteezy.com/ti/vetor-gratis/p1/16546130-compartilhamento-de-dados-e-de-conceito-de-tecnologia-movel-de-pessoas-que-usam-gadgets-como-laptop-pc-e-smartphone-para-compartilhar-postagens-mensagens-e-noticias-em-redes-sociais-ilustracao-em-dos-desenhos-animados-gratis-vetor.jpg" class="img-fluid" alt="...">
+
+                        Ele virou mapa, relógio, câmera, agenda, banco e até lanterna. E tudo isso mostra como a tecnologia, especialmente o celular, está presente em quase tudo que se faz. Ela mudou a forma de viver, e hoje é difícil imaginar o mundo sem ela. <br>
+                        
+                        Portanto, aqui na Live Cell, trazemos e ofertamos apenas os preços mais acessíveis para a aréa da tecnologia, celulares, tablets, notebooks, computadores e mais.
+                        
+                        
+                    </p>
+                <h3><br><br><br>Confira nossos preços, e as melhores promoções: </h3>
+
+                <div class="card my-5">
+                    <img src="https://www.oficinadanet.com.br/imagens/obj_item/1716/xiaomi-redmi-note-13-pro-4g-10.jpg" class="card-img-top " alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Redmi Note 13 Pro 5G 256 GB Preto 8 GB Ram</p>
+                        <a href="#" class="btn btn-primary">R$1097,99</a>
+                    </div>
+                    </div>
+                    <br>
+                    <div class="card my-5">
+                        <img src="https://http2.mlstatic.com/D_NQ_NP_846431-MLB73923301956_012024-O-samsung-galaxy-m62-128gb-8gb-bateria-7000mah-preto-excelente.webp" class="card-img-top " alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Samsung Galaxy M62 128 GB 8 GB RAM</p>
+                            <a href="#" class="btn btn-primary">R$1129,99</a>
+                        </div>
+                    </div>
+                    <div class="container-fluid">
+                        <h4>Sobre nós</h4>
+                        <p>A Live Cell é uma empresa especializada em artigos tecnológicos, oferecendo smartphones, acessórios e dispositivos inteligentes com qualidade e preços justos. Nosso foco é conectar você à tecnologia de forma simples, rápida e segura. Valorizamos o bom atendimento, a confiança e a satisfação em cada compra. Na Live Cell, você encontra inovação, praticidade e uma equipe pronta para te ajudar em cada passo.</p>
+                    </div>
+                    
+                    <div class="container-fluid">
+                        <h4>Avalie-nos</h4>
+                        <form class="border" id="avaliacao">
+                            <div class="form-group">
+                                <label for="exampleFormControlInput1">Endereço de email</label>
+                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nome@exemplo.com">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleFormControlSelect1"> Como nos avalia de 1 a 5</label>
+                                <select class="form-control" id="exampleFormControlSelect1">
+                                <option>1- Péssimo</option>
+                                <option>2- Ruim</option>
+                                <option>3- Razoavel</option>
+                                <option>4- Bom</option>
+                                <option>5- Ótimo</option>
+                                </select>
+                            </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">Deixe aqui seu comentário:</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                    </form>            
+                </div>
+
+            </div>
+            <div class="col-3">
+                <p class="fs-2">Relacionados:</p>
+
+                <div class="card-body border my-5">
+                    <h5 class="card-title">Nvidia</h5>
+                    <p class="card-text">Se você está em busca de placas de vídeo...</p>
+                    <a href="#" class="btn btn-primary">Ver mais</a>
+                </div>
+
+                <div class="card-body border my-5">
+                    <h5 class="card-title">Intel</h5>
+                    <p class="card-text">Umas das maiores empresas no  mercado tecnológico...</p>
+                    <a href="#" class="btn btn-primary">Ver mais</a>
+                </div>
+
+                <div class="card-body border my-5">
+                    <h5 class="card-title">Microsoft</h5>
+                    <p class="card-text">A empresa apresenta o primeiro chip quântico...</p>
+                    <a href="#" class="btn btn-primary">Ver mais</a>
+                </div>
+
+                <div class="card-body border my-5">
+                    <h5 class="card-title">A História da computação</h5>
+                    <p class="card-text">Os primeiros sinais da computação aparecem há milhares de anos, com o uso de ferramentas como o ábaco...</p>
+                    <a href="#" class="btn btn-primary">Ver mais</a>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+    
+    <div class="container-fluid bg-dark py-5 text-light">
+        <div class="row">
+            <div class="col-4">
+                Endereço:
+                Rua paulista, av.534 - Campinas, SP
+
+            </div>
+            <div class="col-4">
+                Cotanto:
+                Telefone: +55(19)98852-2366
+                <br>
+                E-mail: Live.Cell@Live.com
+                <br>
+                Instagram: @Live_Cell
+            </div>
+            <div class="col-4">
+                Dados empresariais:
+                CNPJ: 12.345.678/0001-95
+
+            </div>
+        </div>
+        <br><br>
+        <div class="container-fluid text-center">        Todos os direitos reservados
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
